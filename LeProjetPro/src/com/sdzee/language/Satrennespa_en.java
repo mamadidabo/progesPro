45955886dfaa5f563476418b25a98cf55318593f
Mathieu <Mathieu@148.60.13.96>
2014-02-24 10:25:16 +0100

    Langue

---
+++ LeProjetPro/src/com/sdzee/language/Satrennespa_en.java
@@ -0,0 +1,21 @@
+package com.sdzee.language;
+
+import java.util.ListResourceBundle;
+
+public class Satrennespa_en extends ListResourceBundle{
+
+	public Object[][] getContents() {
+		return contents;
+	}
+	
+	 static final Object[][] contents = {
+		  {"index.menu.titre1","New problem"},
+		  {"index.menu.titre2","Examples"},
+		  {"index.menu.titre2.soustitre1", "Latin square 3x3"},
+		  {"index.menu.titre2.soustitre2", "Sudoku 2x2"},
+		  {"index.menu.titre2.soustitre3", "Sudoku 4x4"},
+		  {"index.menu.titre2.soustitre4", "Sudoku 9x9"},
+		  {"index.menu.titre2.soustitre5", "Sheep/Wolf/Cabbage"},
+
+		};
+}
---
+++ LeProjetPro/src/com/sdzee/language/Satrennespa_fr.java
@@ -0,0 +1,20 @@
+package com.sdzee.language;
+
+import java.util.ListResourceBundle;
+
+public class Satrennespa_fr extends ListResourceBundle{
+	public Object[][] getContents() {
+		return contents;
+	}
+	
+	 static final Object[][] contents = {
+		  {"index.menu.titre1","Nouveau problème"},
+		  {"index.menu.titre2","Exemples"},
+		  {"index.menu.titre2.soustitre1", "Carr&eacute Latin 3x3"},
+		  {"index.menu.titre2.soustitre2", "Sudoku 2x2"},
+		  {"index.menu.titre2.soustitre3", "Sudoku 4x4"},
+		  {"index.menu.titre2.soustitre4", "Sudoku 9x9"},
+		  {"index.menu.titre2.soustitre5", "Mouton/Loup/Chou"},
+
+		};
+}
