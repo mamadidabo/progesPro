903b09ec43f4a9599ca3f18ea625428783608b0a
Mathieu <Mathieu@148.60.13.75>
2014-03-21 09:34:02 +0100

    Effacement fichier LeProjetPro

--- LeProjetPro/src/com/sdzee/dao/UtilisateurDao.java
+++
@@ -1,8 +0,0 @@
-package com.sdzee.dao;
-
-import com.sdzee.beans.Utilisateur;
-public interface UtilisateurDao {
-    void creer( Utilisateur utilisateur ) throws Exception ,DAOException;
-    Utilisateur trouver2( String mot_de_passe) throws Exception, DAOException;  
-    Utilisateur trouver( String email) throws DAOException, Exception;  
-   }
